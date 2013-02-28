a8954169db66f8d2484b29c812ff14526dd8a3ee
Kaique da Silva <kaique.developer@gmail.com>
2013-02-28 03:26:09 -0300

    select attributes

---
+++ index.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';    
+    var root = exports;
+
+    var conscience = root.conscience = require('./lib/conscience'),
+        memory = root.memory = require('./lib/memory');
+}).call(this);
